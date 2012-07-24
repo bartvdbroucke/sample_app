be7ab4b2426bf1750a64baa1365165f4bb429909
Bart Vandenbroucke <youremail@example.com>
2012-07-24 10:41:12 +0200

    Added user microposts

--- db/migrate/20120723071001_add_admin_to_users.rb
+++ db/migrate/20120723071001_add_admin_to_users.rb
@@ -1,5 +1,9 @@
 class AddAdminToUsers < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :users, :admin, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :users, :admin
   end
 end
